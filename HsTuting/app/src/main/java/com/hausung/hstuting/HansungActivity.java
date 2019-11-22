89c6f01682e5aa2ce26204a2e8f75f702b44d16c
yeeun3780 <yeeun1203@naver.com>
2019-11-22 18:44:10 +0900

    최유진 파이어베이스 로그인+회원가입 기능 구현

---
+++ HsTuting/app/src/main/java/com/hausung/hstuting/HansungActivity.java
@@ -0,0 +1,20 @@
+package com.hausung.hstuting;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HansungActivity extends AppCompatActivity {
+
+    Button registerBtn, loginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hansung);
+
+
+    }
+
+
+}
