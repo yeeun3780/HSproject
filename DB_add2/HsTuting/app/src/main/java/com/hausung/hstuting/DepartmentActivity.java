55be6583b4f89cff9a895848c59de7e475d52e6e
Eastsky94 <dongminmin94@gmail.com>
2019-11-25 17:36:26 +0900

    클라우드 파이어스토어에 튜터링 입력 정보 더 추가할 수 있도록 기능 구현

---
+++ DB_add2/HsTuting/app/src/main/java/com/hausung/hstuting/DepartmentActivity.java
@@ -0,0 +1,19 @@
+package com.hausung.hstuting;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DepartmentActivity extends AppCompatActivity {
+
+    Button registerBtn, loginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_department);
+
+
+    }
+
+}
