12901f4dae3793e8c836eb1f0c3478c2bd6c8ac1
yeeun3780 <yeeun1203@naver.com>
2019-12-03 18:04:00 +0900

    채팅방 리스트/채팅방 생성/채팅방/프로필페이지 구현

---
+++ HsTuting_final/app/src/main/java/com/hausung/hstuting/Fragments/APIService.java
@@ -0,0 +1,22 @@
+package com.hausung.hstuting.Fragments;
+
+
+import com.hausung.hstuting.Notifications.MyResponse;
+import com.hausung.hstuting.Notifications.Sender;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface APIService {
+    @Headers(
+            {
+                    "Content-Type:application/json",
+                    "Authorization:key=ADD HERE YOUR KEY FROM FIREBASE PROJECT"
+            }
+    )
+
+    @POST("fcm/send")
+    Call<MyResponse> sendNotification(@Body Sender body);
+}
---
+++ HsTuting_fragment/app/src/main/java/com/hausung/hstuting/Usermodel.java
@@ -0,0 +1,26 @@
+package com.hausung.hstuting;
+
+public class Usermodel {
+
+    private String user_rank;
+    private String user_time;
+    private String userphoto;
+    private String token;
+
+    public String getUser_rank(){ return user_rank;}
+
+    public void setUser_rank(String user_rank){this.user_rank = user_rank;}
+
+    public String getUser_time(){ return user_time;}
+
+    public void setUser_time(String user_time){this.user_time = user_time;}
+
+    public String getUserphoto(){ return userphoto;}
+
+    public void setUserphoto(String userphoto){this.userphoto = userphoto;}
+
+    public String getToken(){ return token;}
+
+    public void setToken(String token){this.token = token;}
+
+}
